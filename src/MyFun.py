4d0ef3b131c70e88b8a73e8f4d74771411eab26c
Mephistofer <Mephistof@163.com>
2018-02-26 09:29:55 +0800

    1

--- src/MyFun.py
+++ src/MyFun.py
@@ -4,22 +4,27 @@ Created on 2017年9月19日
 
 @author: Saya
 '''
+
+
 def make_pizza(size, *toppings):
     print("\nMaking a " + str(size) +
-     "-inch pizza with the following toppings:")
+          "-inch pizza with the following toppings:")
     for topping in toppings:
         print("- " + topping)
 
-def describe_pet( pet_name, animal_type='dog'):
+
+def describe_pet(pet_name, animal_type='dog'):
     print("\nI have a " + animal_type + ".")
     print("My " + animal_type + "'s name is " + pet_name.title() + ".")
 
-         
+
 class Dog():
     def __init__(self, name, age):
         self.name = name
         self.age = age
+
     def sit(self):
-         print(self.name.title() + " is now sitting.")
+        print(self.name.title() + " is now sitting.")
+
     def roll_over(self):
         print(self.name.title() + " rolled over!")
